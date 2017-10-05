ee645558ce026cfa03909ebb3c2f8df6c41d2e20
Grahame Bowland <grahame@angrygoats.net>
2017-10-05 12:12:53 +0800

    run spreadsheet directly

--- bpaworkflow/bpaworkflow/validate.py
+++ bpaworkflow/bpaworkflow/validate.py
@@ -13,30 +13,41 @@ def write_file(tempd, file_obj):
     name = os.path.basename(file_obj.name)
     if not name:
         raise Exception("invalid filename provided in upload")
-    with open(os.path.join(tempd, name), 'wb') as fd:
+    fpath = os.path.join(tempd, name)
+    with open(fpath, 'wb') as fd:
         for chunk in file_obj.chunks():
             fd.write(chunk)
+    return fpath
+
+
+def verify_spreadsheet(cls, fpath, metadata_info):
+    rows = cls.parse_spreadsheet(fpath, metadata_info)
+    logger.critical(len(rows))
 
 
 def run_validator(cls, files):
     with TemporaryDirectory(prefix='bpaworkflow') as tempd:
+        paths = {}
         for field_name, file_obj in files.items():
             logger.critical('writing a file: %s' % repr(file_obj))
-            write_file(tempd, file_obj)
+            paths[field_name] = write_file(tempd, file_obj)
 
-        # we don't want the project metadata to download, so we
-        # clear the project metadata URLs
-        cls.metadata_urls = []
         # fabricate metadata information for the files uploaded by the user
         metadata_info = {}
         for filename in os.listdir(tempd):
+            # synthesise harmless additional context data so we can run the ingestor
             metadata_info[os.path.basename(filename)] = obj = {
                 "base_url": "https://example.com/does-not-exist/",
             }
             for k in cls.metadata_url_components:
                 obj[k] = 'BPAOPS-999'
 
+        verify_spreadsheet(cls, paths['xlsx'], metadata_info)
+
+        # we don't want the project metadata to download, so we
+        # clear the project metadata URLs
+        cls.metadata_urls = []
+
         with DownloadMetadata(cls, path=tempd, force_fetch=True, metadata_info=metadata_info) as dlmeta:
             logger.critical(dlmeta.meta)
-            dlmeta.meta.get_packages()
             dlmeta.meta.get_resources()
